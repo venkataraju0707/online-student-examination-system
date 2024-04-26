d7ce25c784c15e10b33aa504324a986a9e79e129
VENKATARAJU <161044578+venkataraju0707@users.noreply.github.com>
2024-04-26 21:12:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Online Examination system</title>
+<link rel="stylesheet" href="style.css">
+<link href='https://fonts.googleapis.com/css?family=EB Garamond' rel='stylesheet'>
+</head>
+<body>
+<section class="header">
+   <nav>
+     <a href="indext.html"><img src="logo1.jpeg"></a>
+     <h1 class="head">EXAMINATION MANAGEMENT</h1>
+    </nav>	 
+     <ul class="nav-links">
+	 <li><a href="indext.html">HOME</a></li>
+	 <li><a href="about.html">ABOUT</a></li>
+	 <li><a href="login.html">LOGIN</a></li>
+	 <li><a href="contact.html">CONTACT-US</a></li>
+    </ul>
+   <p class="detail">Exams are the most fearful and stretching word in academic life! The examination management is equally
+   stretching for management and staff as it requries a lot of resouces and effort to conduct the exams sucessfully.
+   Making the examinations eco-friendly by adopting the digital form of examination management.A one-time investment will
+   help the institute to adopt small steps towards environmental protection.The computerized form helps to 
+   achieve less or zero paper usage.Online Exam Management has become a requisite in contemporary educational and professional 
+   landscapes owing to its multifaceted advantages that transcend the limitations of traditional pen-and-paper examinations. Firstly, it
+   streamlines the entire examination process, alleviating the administrative burden on educational institutions and allowing them to focus on
+   more meaningful aspects of education. The digital transformation facilitates the creation, distribution, and evaluation of exams with 
+   unparalleled efficiency. Moreover, the automated grading system not only saves 
+   time but also reduces the likelihood of errors associated with manual grading.</p>
+   <p class="detail">Secondly, online exam management enhances accessibility by breaking down geographical barriers. Students can take exams from
+   the comfort of their homes or any location with internet access, fostering inclusivity and accommodating learners who may face challenges attending
+   physical exam venues. This is particularly relevant in the era of global connectivity and the increasing prevalence of online education.
+   The incorporation of advanced security features is another compelling reason for the adoption of online exam management systems. These systems
+   employ technologies like facial recognition, biometrics, and secure browser environments to ensure the integrity of the examination process. Remote
+   proctoring tools allow for real-time monitoring, mitigating the risk of cheating and malpractice.Additionally, the system provides a comprehensive and 
+   centralized repository for storing and managing a vast array of questions, allowing for efficient organization and reuse. This question bank feature not
+   only saves time for educators but also ensures a diverse and fair assessment for students. The randomization of questions and answer choices adds an 
+   extra layer of fairness by preventing cheating through memorization.Online exam management systems contribute to a more environmentally friendly approach
+   by reducing the need for excessive paper usage. This aligns with the global push towards sustainability and digitalization in various sectors.</p><br>
+   </section>
+   <section class="header1">
+    <h1>....CONTACT DETAILS....</h1>
+   <div class="row">
+   <div class="de-col">
+   <h2 class="details">...ADDRESS...</h2>
+   <p>Gayatri Vidya Parishad College for <br> Degree and P.G Courses(A),<br>Engineering and Technology.<br> Rushikonda,<br> Visakhapatnam-530045,<br> Andhra Pradesh.</p>
+   </div>
+   <div class="vli"></div>
+   <div class="de-col">
+   <h2 class="details">...CONTACT-US...</h2>
+   <p>principalgvpcdpgca@gmail.com,<br>Phone No: 0891-2955084,<br>Working Hours:<br>Mon-Sat 9:00 AM - 5:00 PM<br>Phone No 0891-2953719 (E&T).</p>
+   </div>
+   
+   </section>
+  </body>
+</html>
